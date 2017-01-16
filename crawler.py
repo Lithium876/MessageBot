fabb9f335a72edd009d60c9a7f291c8b1c456819
unknown <lithiumlilly3@gmail.com>
2017-01-15 23:47:58 -0500

    Added CSV File Reader

--- crawler.py
+++ crawler.py
@@ -1,4 +1,17 @@
 import optparse
+import csv
+
+arr = []
+def openFile(options):
+	with open(options,'r') as csvfile:
+		csvreader = csv.reader(csvfile, delimiter=',')
+		for row in csvreader:
+			arr.append(row)
+		URL = arr[0][1]
+		num_matches_to_visit = arr[1][1]
+		message_file = arr[2][1]
+		max_wait = arr[3][1]
+		#print("\nURL: %s\n\nNumber of Matches to Vist: %s\nMassage File: %s\nMax Wait: %s"%(URL,num_matches_to_visit,message_file,max_wait))
 
 def main():
 	parser = optparse.OptionParser('--username <USERNAME> --password <PASSWORD> --options <OPTIONFILENAME.CSV>', version="%prog 1.0")
@@ -13,7 +26,8 @@ def main():
 		username = options.username
 		password = options.password
 		options = options.options
-		print("\nUsername: %s \nPassword: %s \nOption: %s"%(username, password, options))
+		#print("\nUsername: %s \nPassword: %s \nOption: %s"%(username, password, options))
+		openFile(options)
 
 if __name__ == '__main__':
 	main()
